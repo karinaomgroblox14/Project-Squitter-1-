8258705f631271cc48392a2a920b98eb731a95f5
Karingomgroblox14 <74888422+karinaomgroblox14@users.noreply.github.com>
2021-04-25 16:23:33 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+var firebaseConfig = {
+      apiKey: "AIzaSyBRg3JnfjV9Mz0JFsTi86JrJQbwAO6UYiY",
+      authDomain: "kwitter-750a1.firebaseapp.com",
+      projectId: "kwitter-750a1",
+      storageBucket: "kwitter-750a1.appspot.com",
+      messagingSenderId: "811377002782",
+      appId: "1:811377002782:web:89555926d5a764cbfe610f"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      
+      //End code
+      });});}
+getData();
